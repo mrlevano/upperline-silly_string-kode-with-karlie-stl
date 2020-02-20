9f0eccd0289aa87dd72d5834793c529b7a8fad20
Paolo Speziali <speziali99@gmail.com>
2020-02-20 23:06:00 +0100

    Update silly_string.rb
    m'agg scurdat ajajajaj

--- silly_string.rb
+++ silly_string.rb
@@ -6,7 +6,8 @@ var = "Staccoverflo"
 #2: Using a method built into ruby, capitalize every letter in the string you just created
 
 #3: Now let's make all the characters lowercase in the string below:
-puts "IM NOT SHOUTING".downcase
+puts "IM NOT SHOUTING"
+puts "im not shouting"
 
 #4: Using a method built into ruby, count how many letters are in the string below:
 a="supercalifragilisticexpialidocious".chars
@@ -35,6 +36,8 @@ puts a
 #6. Let's capitalize the first character of the string below:
 
 puts "i really like programming"
+
+puts "I really like programming"
 
 #7 Replace all "A" with "O" and "a" with "o" by chaining together `gsub`
 puts "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
